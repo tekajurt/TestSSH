79341a1afba039298d07c1b36cc4d13040cd90b8
tekajurt <danielalejandro.hurtado@alumnos.ulagos.cl>
2023-01-14 02:48:55 -0300

    server?

--- plataformavoluntarios/next.config.js
+++ plataformavoluntarios/next.config.js
@@ -1,13 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  }
+  reactStrictMode: true
+  
 }
 
 module.exports = nextConfig
